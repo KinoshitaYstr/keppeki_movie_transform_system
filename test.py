ef3ba73f1fa976f2c10fa90436226c621b85d2d3
KinoshitaYstr <photo_18@outlook.jp>
2020-07-01 23:02:56 +0900

    update

--- test.py
+++ test.py
@@ -1,14 +1,33 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import QCoreApplication
+#!/usr/bin/env python3
 
-class Example(QWidget):
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QToolTip, QPushButton, QMessageBox, QAction, qApp, QTextEdit
+from PyQt5.QtGui import QFont, QIcon
+from PyQt5.QtCore import QCoreApplication, Qt
+
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
     
     def initUI(self):
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exitAction = QAction(QIcon('lenna.gif'), '&Exif', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        self.statusBar().showMessage('Ready')
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAction)
+
         QToolTip.setFont(QFont('SansSerif', 10))
         self.setToolTip('This is a <b>QWidget</b> widget')
         
@@ -17,11 +36,22 @@ class Example(QWidget):
         btn.setToolTip('This is a <b>Quit Button</b> widget')
 
         btn.resize(btn.sizeHint())
-        btn.move(50, 50)
+        btn.move(50, 100)
 
         self.setGeometry(300, 300, 300, 200)
         self.setWindowTitle("Example")
         self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', 'Are you sure to quit?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+    
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
